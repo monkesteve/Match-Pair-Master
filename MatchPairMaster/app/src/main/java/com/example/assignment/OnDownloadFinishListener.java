a44e848ec54c4684d2455153c58de5f6fe10d7e8
steveglowplunk <28670916+steveglowplunk@users.noreply.github.com>
2025-04-06 07:50:55 +0800

    make runnable

--- MatchPairMaster/app/src/main/java/com/example/assignment/OnDownloadFinishListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.assignment;
-
-public interface OnDownloadFinishListener {
-    public void updateDownloadResult(String result);
-}
--- MatchPairMaster/app/src/main/java/com/example/assignment/OnRecyclerViewClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.assignment;
-
-import android.view.View;
-
-public interface OnRecyclerViewClickListener {
-    void onItemClickListener(View view);
-}
